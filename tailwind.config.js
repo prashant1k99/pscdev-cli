8d2483ca0010545aac70a70bf7b15edee202b892
Prashant <prashantco111@gmail.com>
2024-07-14 21:50:11 +0530

    chore: Add SvelteKit layout and page components

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export const content = ['./src/**/*.{html,js,svelte,ts}'];
+export const theme = {
+  extend: {
+    fontFamily: {
+      mono: ['Consolas', 'Monaco', 'Lucida Console', 'Liberation Mono', 'DejaVu Sans Mono', 'Bitstream Vera Sans Mono', 'Courier New', 'monospace'],
+    },
+    colors: {
+      'terminal-black': '#2e3436',
+      'terminal-green': '#4e9a06',
+    },
+  },
+};
+export const plugins = [];
